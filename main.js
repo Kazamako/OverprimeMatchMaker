2dd5cdaed952f4e8a69fae070b2543166a8929e1
Kazamako <97965122+Kazamako@users.noreply.github.com>
2022-01-20 21:55:40 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ function randomMatch(){
   var ctx = canvas.getContext('2d');
   ctx.clearRect(0, 0, 500, 200);
   ctx.drawImage(bgImg, 0, 0)
-  ctx.font = '20px Arial';
+  ctx.font = '20px sans-serif';
   ctx.fillStyle = '#87ceeb';
   ctx.textBaseline = 'top';
   ctx.textAlign = 'left';
@@ -62,11 +62,11 @@ function randomMatch(){
   ctx.fillText(randomlist[2][1], alphax1, 110);
   ctx.fillText(randomlist[3][1], alphax1, 140);
   ctx.fillText(randomlist[4][1], alphax1, 170);
-  ctx.fillText(randomlist[0][0].substr(0,9), alphax2, 50);
-  ctx.fillText(randomlist[1][0].substr(0,9), alphax2, 80);
-  ctx.fillText(randomlist[2][0].substr(0,9), alphax2, 110);
-  ctx.fillText(randomlist[3][0].substr(0,9), alphax2, 140);
-  ctx.fillText(randomlist[4][0].substr(0,9), alphax2, 170);
+  ctx.fillText(randomlist[0][0], alphax2, 50, 150);
+  ctx.fillText(randomlist[1][0], alphax2, 80, 150);
+  ctx.fillText(randomlist[2][0], alphax2, 110, 150);
+  ctx.fillText(randomlist[3][0], alphax2, 140, 150);
+  ctx.fillText(randomlist[4][0], alphax2, 170, 150);
   var bravox1 = 260;
   var bravox2 = 320;
   ctx.fillStyle = '#f08080';
@@ -76,11 +76,11 @@ function randomMatch(){
   ctx.fillText(randomlist[7][1], bravox1, 110);
   ctx.fillText(randomlist[8][1], bravox1, 140);
   ctx.fillText(randomlist[9][1], bravox1, 170);
-  ctx.fillText(randomlist[5][0].substr(0,9), bravox2, 50);
-  ctx.fillText(randomlist[6][0].substr(0,9), bravox2, 80);
-  ctx.fillText(randomlist[7][0].substr(0,9), bravox2, 110);
-  ctx.fillText(randomlist[8][0].substr(0,9), bravox2, 140);
-  ctx.fillText(randomlist[9][0].substr(0,9), bravox2, 170);
+  ctx.fillText(randomlist[5][0], bravox2, 50, 150);
+  ctx.fillText(randomlist[6][0], bravox2, 80, 150);
+  ctx.fillText(randomlist[7][0], bravox2, 110, 150);
+  ctx.fillText(randomlist[8][0], bravox2, 140, 150);
+  ctx.fillText(randomlist[9][0], bravox2, 170, 150);
 
   // テキスト版のチーム分けも更新（非表示）
   document.getElementById("kekka").value =
@@ -104,7 +104,7 @@ function eloMatch(){
   var userlist = [];
   var randomELOUserList = [];
   var goodTeamList = [0,0,0,0,0,0,0,0,0,0];
-  var goodTeamELOdiff = 9999;
+  var goodTeamELOdiff = 99999999;
   var goodTeamAlphaELO = 0;
   var goodTeamBravoELO = 0;
 
@@ -173,7 +173,7 @@ function eloMatch(){
   var ctx = canvas.getContext('2d');
   ctx.clearRect(0, 0, 500, 200);
   ctx.drawImage(bgImg, 0, 0)
-  ctx.font = '20px Arial';
+  ctx.font = '20px sans-serif';
   ctx.fillStyle = '#87ceeb';
   ctx.textBaseline = 'top';
   ctx.textAlign = 'left';
@@ -189,7 +189,7 @@ function eloMatch(){
   for(var i = 0; i < goodTeamList.length; i++){
     if(goodTeamList[i] == 0){
       ctx.fillText(userlist[i][1], alphax1, y);
-      ctx.fillText(userlist[i][0].substr(0,9), alphax2, y);
+      ctx.fillText(userlist[i][0], alphax2, y, 150);
       y = y + 30;
     }
   }
@@ -199,7 +199,7 @@ function eloMatch(){
   for(var i = 0; i < goodTeamList.length; i++){
     if(goodTeamList[i] == 1){
       ctx.fillText(userlist[i][1], bravox1, y);
-      ctx.fillText(userlist[i][0].substr(0,9), bravox2, y);
+      ctx.fillText(userlist[i][0], bravox2, y, 150);
       y = y + 30;
     }
   }
